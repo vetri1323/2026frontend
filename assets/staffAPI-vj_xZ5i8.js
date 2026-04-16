422811bd4307ca6775848d3cd0c382a02ed4e421
vetri1323 <vetrigp@gmail.com>
2026-04-16 14:10:13 +0530

    Add files via upload

---
+++ assets/staffAPI-vj_xZ5i8.js
@@ -0,0 +1,2 @@
+import{a as e}from"./index-DIoo9JmW.js";import"./chart-CqQliw9m.js";import"./router-D4AvwfLG.js";import"./utils-Bs_CWHHL.js";const i={getAll:()=>e.get("/staff"),getById:t=>e.get(`/staff/${t}`),create:t=>e.post("/staff",t),update:(t,f)=>e.put(`/staff/${t}`,f),delete:t=>e.delete(`/staff/${t}`),getActive:()=>e.get("/staff/active")};export{i as staffAPI};
+//# sourceMappingURL=staffAPI-vj_xZ5i8.js.map
