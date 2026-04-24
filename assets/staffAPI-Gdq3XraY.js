b1b36ca50b7c4c580a2f63b1c8a74e2abbc379fd
vetri1323 <vetrigp@gmail.com>
2026-04-25 00:00:11 +0530

    Add files via upload

---
+++ assets/staffAPI-Gdq3XraY.js
@@ -0,0 +1,2 @@
+import{a as e}from"./index-DokcVZ5z.js";import"./chart-_Gz3zQ8t.js";import"./router-DD_4mbHH.js";import"./utils-iSGX7wc3.js";const i={getAll:()=>e.get("/staff"),getById:t=>e.get(`/staff/${t}`),create:t=>e.post("/staff",t),update:(t,f)=>e.put(`/staff/${t}`,f),delete:t=>e.delete(`/staff/${t}`),getActive:()=>e.get("/staff/active")};export{i as staffAPI};
+//# sourceMappingURL=staffAPI-Gdq3XraY.js.map
